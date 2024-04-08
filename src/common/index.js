3f2845fe621b9af04f3940e39a86d2b5a6eb258a
Zeenat <zeenatzfq052@gmail.com>
2024-04-08 23:08:12 +0530

    base url

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,6 @@
-const backendDomin = "http://localhost:8080"
-
+// const backendDomin = "http://localhost:8080"
+const backendDomin = process.env.REACT_APP_BACKEND_BASE_URL
+console.log(backendDomin,"backendDomin");
 const SummaryApi = {
     signUP : {
         url : `${backendDomin}/api/signup`,
